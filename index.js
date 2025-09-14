d65c9538c118e4a5b4b6622e7e582d2b6bf3e8f0
Dibesh Ranjan <dibeshranjan6@gmail.com>
2025-09-14 17:06:35 +0530

    update routes

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema(
     password: { type: String, required: true },
     role: { type: String, default: "customer" }, // customer | reporter
   },
-  { versionKey: false }
+  { versionKey: false, collection : "newsUser" }
 );
 const User = mongoose.model("User", userSchema);
 
