2286dec224deb20b65d0ee9f4b34477ca0a2cb2f
Dibesh Ranjan <dibeshranjan6@gmail.com>
2025-09-17 00:23:22 +0530

    update routes

--- index.js
+++ index.js
@@ -143,16 +143,16 @@ app.post("/api/auth/login", async (req, res) => {
 // });
 
 // /* ================== PROFILE ================== */
-// app.get("/api/users/profile", protect, async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user.id).select("-password");
-//     if (!user) return res.status(404).json({ message: "User not found" });
+app.get("/api/users/profile", protect, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
 
-//     res.json(user);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 /* ================== POST ROUTES ================== */
 
